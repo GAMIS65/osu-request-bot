88e8be6feb7c49458374eecafee049dbb3b3e640
GAMIS65 <ivicko05@gmail.com>
2022-11-09 16:08:38 +0100

    chore: Move tmi client to a different file

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,20 @@
 import { ChatUserstate } from 'tmi.js';
-import { getMapLinkFromMessage, getDiffFromMessage } from './twitch';
+import { client as tmiClient , getMapLinkFromMessage, getDiffFromMessage } from './twitch';
 import { getMap } from './map-processor';
 
 process.on('uncaughtException', function (err) {
     console.log(err.message);
 });
 
-const tmi = require('tmi.js');
-
-const client = new tmi.Client({
-    connection: {
-        secure: true,
-        reconnect: true
-    },
-    channels: ['gamis65'] // TODO: Get channels from DB
-});
-
-client.connect().then(() => {
+tmiClient.connect().then(() => {
     console.log('Connected to Twitch!');
 }).catch(console.error);
 
-client.on('message', async (channel: string, tags: ChatUserstate, message: string, self: boolean) => {
+tmiClient.on('message', async (channel: string, tags: ChatUserstate, message: string, self: boolean) => {
     const mapLink = getMapLinkFromMessage(message);
     if(!mapLink) return;
 
-    const mapId = getDiffFromMessage(message)
+    const mapId = getDiffFromMessage(message);
     if(!mapId) return;
 
     const beatmap = await getMap(mapId);
--- src/twitch.ts
+++ src/twitch.ts
@@ -1,3 +1,13 @@
+const tmi = require('tmi.js');
+
+export const client = new tmi.Client({
+    connection: {
+        secure: true,
+        reconnect: true
+    },
+    channels: ['gamis65'] // TODO: Get channels from DB
+});
+
 export const getMapLinkFromMessage = (message: string): string | undefined  => {
     const linkRegex = '(https:\/\/osu\.ppy\.sh\/beatmapsets\/)(.+[0-9])';
     let map = '';
