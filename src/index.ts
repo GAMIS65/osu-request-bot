f9cbb2972a41fb85169ab72a7ddb15913189bd6d
GAMIS65 <ivicko05@gmail.com>
2022-11-09 17:55:14 +0100

    refactor: Move tmi client stuff to twitch

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,8 @@
-import { ChatUserstate } from 'tmi.js';
-import { client as tmiClient , getMapLinkFromMessage, getDiffFromMessage } from './twitch';
-import { getMap } from './map-processor';
+const Bancho = require("./bancho");
+const Twitch = require("./twitch");
+
+console.log('Attempting to connect to Twitch and Bancho...')
 
 process.on('uncaughtException', function (err) {
     console.log(err.message);
 });
-
-tmiClient.connect().then(() => {
-    console.log('Connected to Twitch!');
-}).catch(console.error);
-
-tmiClient.on('message', async (channel: string, tags: ChatUserstate, message: string, self: boolean) => {
-    const mapLink = getMapLinkFromMessage(message);
-    if(!mapLink) return;
-
-    const mapId = getDiffFromMessage(message);
-    if(!mapId) return;
-
-    const beatmap = await getMap(mapId);
-    if(beatmap) console.log(`${tags['display-name']} requested ${beatmap.title} ${Math.round(beatmap.difficulty.rating * 100) / 100}* ${beatmap.bpm} BPM`);
-})
--- src/twitch.ts
+++ src/twitch.ts
@@ -1,4 +1,6 @@
 const tmi = require('tmi.js');
+import { ChatUserstate } from 'tmi.js';
+import { getMap } from './map-processor';
 
 export const client = new tmi.Client({
     connection: {
@@ -22,6 +24,20 @@ export const getDiffFromMessage = (message: string): string | undefined => {
     const diffRegex = '(?<=#osu\/).*$';
     let map = message.match(diffRegex);
     // @ts-ignore
-    let mapId = map[0];
-    return mapId;
+    return map[0];
 }
+
+client.connect().then(() => {
+    console.log('Connected to Twitch!');
+}).catch(console.error);
+
+client.on('message', async (channel: string, tags: ChatUserstate, message: string, self: boolean) => {
+    const mapLink = getMapLinkFromMessage(message);
+    if(!mapLink) return;
+
+    const mapId = getDiffFromMessage(message);
+    if(!mapId) return;
+
+    const beatmap = await getMap(mapId);
+    if(beatmap) console.log(`${tags['display-name']} requested ${beatmap.title} ${Math.round(beatmap.difficulty.rating * 100) / 100}* ${beatmap.bpm} BPM`);
+})
