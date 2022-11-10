af112c79ea388bf61b2f1e6c6e4fb9f07f43e765
GAMIS65 <ivicko05@gmail.com>
2022-11-10 15:55:04 +0100

    feat: Basic Viewer Validation

--- src/twitch.ts
+++ src/twitch.ts
@@ -2,7 +2,7 @@ const tmi = require('tmi.js');
 import { ChatUserstate } from 'tmi.js';
 import { getMap } from './map-processor';
 
-export const client = new tmi.Client({
+const client = new tmi.Client({
     connection: {
         secure: true,
         reconnect: true
@@ -10,7 +10,7 @@ export const client = new tmi.Client({
     channels: ['gamis65'] // TODO: Get channels from DB
 });
 
-export const getMapLinkFromMessage = (message: string): string | undefined  => {
+const getMapLinkFromMessage = (message: string): string | undefined  => {
     const linkRegex = '(https:\/\/osu\.ppy\.sh\/beatmapsets\/)(.+[0-9])';
     let map = '';
 
@@ -20,11 +20,29 @@ export const getMapLinkFromMessage = (message: string): string | undefined  => {
     }
 }
 
-export const getDiffFromMessage = (message: string): string | undefined => {
+const getDiffFromMessage = (message: string): string | undefined => {
     const diffRegex = '(?<=#osu\/).*$';
     let map = message.match(diffRegex);
     // @ts-ignore
     return map[0];
+}
+
+const getViewerType = (viewer: ChatUserstate): string => {
+    if(viewer.badges && viewer.badges.broadcaster) return 'Broadcaster';
+    if(viewer.mod) return 'Mod';
+    if(viewer.badges && viewer.badges.vip) return 'VIP';
+    if(viewer.subscriber) return 'Sub';
+    return 'Viewer';
+}
+
+const checkViewerPerms = (viewer: ChatUserstate, viewerType: string) => {
+    // TODO: Add these things to DB
+    let isBlacklisted = false; 
+    let subOnly = false;
+
+    if(isBlacklisted) return false;
+    if(subOnly && viewerType === 'Viewer') return false; // TODO: Change this to check an array of allowed viewer types instead
+    return true;
 }
 
 client.connect().then(() => {
@@ -35,9 +53,13 @@ client.on('message', async (channel: string, tags: ChatUserstate, message: strin
     const mapLink = getMapLinkFromMessage(message);
     if(!mapLink) return;
 
+    const viewerType = getViewerType(tags);
+    const canViewerRequest = checkViewerPerms(tags, viewerType);
+    if(!canViewerRequest) return;
+
     const mapId = getDiffFromMessage(message);
     if(!mapId) return;
 
     const beatmap = await getMap(mapId);
-    if(beatmap) console.log(`${tags['display-name']} requested ${beatmap.title} ${Math.round(beatmap.difficulty.rating * 100) / 100}* ${beatmap.bpm} BPM`);
+    if(beatmap) console.log(`[${viewerType}] ${tags['display-name']} requested ${beatmap.title} ${Math.round(beatmap.difficulty.rating * 100) / 100}* ${beatmap.bpm} BPM`);
 })
