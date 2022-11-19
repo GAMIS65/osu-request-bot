87934e174536905725cb9f02cde186a10ad51f93
GAMIS65 <ivicko05@gmail.com>
2022-11-19 10:44:10 +0100

    refactor: Better command handling

--- src/bancho.ts
+++ src/bancho.ts
@@ -8,6 +8,21 @@ export const sendBanchoMessage = async(user: string | number, text: string) => {
 	await recipient.sendMessage(text);
 }
 
+const register = async (message: BanchoMessage, twitchUsername: string) => {
+	try {
+		// TODO: Verify twitch account
+		if(twitchUsername) {
+			await addUser(message.user.id, message.user.ircUsername, twitchUsername)
+			await sendBanchoMessage(message.user.ircUsername, `Registered successfully! The bot will listen for requests in a minute or two. Type !help for help`);
+		} else {
+			await sendBanchoMessage(message.user.ircUsername, `You forgot to add your twitch username!`);
+		}
+	} catch (e: any) {
+		if (e.code === 11000) return await sendBanchoMessage(message.user.ircUsername, `You are already registered! Type !help for help`);
+		await sendBanchoMessage(message.user.ircUsername, `Something went wrong! ${e}`);
+	}
+}
+
 const client = new Banchojs.BanchoClient({ username: process.env.BOT_OSU_USERNAME, password: process.env.OSU_IRC_PASSWORD });
 client.connect().then(() => {
 	console.log('Connected to Bancho!');
@@ -15,18 +30,16 @@ client.connect().then(() => {
 
 client.on('PM', async (message: BanchoMessage) => {
 	const splitMessage = message.content.split(" ");
-	if(splitMessage[0] === '!register') {
-		try {
-			// TODO: Verify twitch account
-			if(splitMessage[1]) {
-				await addUser(message.user.id, message.user.ircUsername, splitMessage[1])
-				await sendBanchoMessage(message.user.ircUsername, `Registered successfully! The bot will listen for requests in a minute or two. Type !help for help`);
-			} else {
-				await sendBanchoMessage(message.user.ircUsername, `You forgot to add your twitch username!`);
-			}
-		} catch (e: any) {
-			if (e.code === 11000) return await sendBanchoMessage(message.user.ircUsername, `You are already registered! Type !help for help`);
-			await sendBanchoMessage(message.user.ircUsername, `Something went wrong! ${e}`);
-		}
+	switch (splitMessage[0]) {
+		case '!help':
+			await sendBanchoMessage(message.user.ircUsername, '[https://github.com/GAMIS65/osu-request-bot/blob/main/README.md#osu-request-bot List of commands]');
+			break;
+
+		case '!register':
+			await register(message, splitMessage[1]);
+			break;
+		
+		default:
+			break;
 	}
 });
